bc3376ea7f1a06938dcab90eab4e309b24e9cb8f
FelipepFranco <80927654+FelipepFranco@users.noreply.github.com>
2022-08-24 10:59:04 -0300

    Add files via upload

---
+++ js/forca.js
@@ -0,0 +1,393 @@
+let tentativas = 6;
+let listaDinamica= [];
+let palavraSecretaCategoria;
+let palavraSecretaSorteada;
+const palavras = [
+  // LUGARES
+  palavra001 = {
+    nome:"IRLANDA",
+    categoria: "LUGARES"
+  },
+  palavra002 = {
+    nome:"EQUADOR",
+    categoria: "LUGARES"
+  },
+  palavra003 = {
+    nome:"CHILE",
+    categoria: "LUGARES"
+  },
+  palavra004 = {
+    nome:"INDONESIA",
+    categoria: "LUGARES"
+  },
+  palavra005 = {
+    nome:"MALDIVAS",
+    categoria: "LUGARES"
+  },
+  palavra006 = {
+    nome:"INGLATERRA",
+    categoria: "LUGARES"
+  },
+  palavra007 = {
+    nome:"GROELANDIA",
+    categoria: "LUGARES"
+  },
+  palavra008 = {
+    nome:"UZBEQUISTAO",
+    categoria: "LUGARES"
+  },
+  palavra009 = {
+    nome:"CANADA",
+    categoria: "LUGARES"
+  },
+  palavra010 = {
+    nome:"HOLANDA",
+    categoria: "LUGARES"
+  },
+  // TRANSPORTES
+
+  palavra011 = {
+    nome:"BICICLETA",
+    categoria: "TRANSPORTES"
+  },
+  palavra012 = {
+    nome:"LANCHA",
+    categoria: "TRANSPORTES"
+  },
+  palavra013 = {
+    nome:"BARCO",
+    categoria: "TRANSPORTES"
+  },
+  palavra014 = {
+    nome:"TREM",
+    categoria: "TRANSPORTES"
+  },
+  palavra015 = {
+    nome:"CARRO",
+    categoria: "TRANSPORTES"
+  },
+  palavra016 = {
+    nome:"MOTOCICLETA",
+    categoria: "TRANSPORTES"
+  },
+  palavra017 = {
+    nome:"SKATE",
+    categoria: "TRANSPORTES"
+  },
+  palavra018 = {
+    nome:"PATINS",
+    categoria: "TRANSPORTES"
+  },
+  palavra019 = {
+    nome:"PATINETE",
+    categoria: "TRANSPORTES"
+  },
+  palavra020 = {
+    nome:"AERONAVE",
+    categoria: "TRANSPORTES"
+  },
+  palavra021 = {
+    nome:"AERONAVE",
+    categoria: "TRANSPORTES"
+  },
+  palavra021 = {
+    nome: "XICARA",
+    categoria:"OBJETOS"
+},
+palavra022 = {
+    nome: "MOEDA",
+    categoria:"OBJETOS"
+},
+palavra023 = {
+    nome: "ESPARADRAPO",
+    categoria:"OBJETOS"
+},
+palavra024 = {
+    nome: "SINO",
+    categoria:"OBJETOS"
+},
+palavra025 = {
+    nome: "CHUVEIRO",
+    categoria:"OBJETOS"
+},
+palavra026 = {
+    nome: "TAMBORETE",
+    categoria:"OBJETOS"
+},
+palavra027 = {
+    nome: "LAMPADA",
+    categoria:"OBJETOS"
+},
+palavra028 = {
+    nome: "BOCAL",
+    categoria:"OBJETOS"
+},
+palavra029 = {
+    nome: "CORTINA",
+    categoria:"OBJETOS"
+},
+palavra030 = {
+    nome: "LAPIS",
+    categoria:"OBJETOS"
+},
+palavra031 = {
+    nome: "MELANCIA",
+    categoria:"ALIMENTOS"
+},
+palavra032 = {
+    nome: "AMENDOIM",
+    categoria:"ALIMENTOS"
+},
+palavra033 = {
+    nome: "ESFIRRA",
+    categoria:"ALIMENTOS"
+},
+palavra034 = {
+    nome: "GOROROBA",
+    categoria:"ALIMENTOS"
+},
+palavra035 = {
+    nome: "JANTAR",
+    categoria:"ALIMENTOS"
+},
+palavra036 = {
+    nome: "SABOROSO",
+    categoria:"ALIMENTOS"
+},
+palavra037 = {
+    nome: "DESJEJUM",
+    categoria:"ALIMENTOS"
+},
+palavra038 = {
+    nome: "MASTIGAR",
+    categoria:"ALIMENTOS"
+},
+palavra039 = {
+    nome: "ENGOLIR",
+    categoria:"ALIMENTOS"
+},
+palavra040 = {
+    nome: "DOCERIA",
+    categoria:"ALIMENTOS"
+},
+palavra040 = {
+    nome: "DRAGAO",
+    categoria:"ANIMAIS"
+},
+palavra041 = {
+    nome: "GALINHA",
+    categoria:"ANIMAIS"
+},
+palavra042 = {
+    nome: "PAVAO",
+    categoria:"ANIMAIS"
+},
+palavra043 = {
+    nome: "CAMELO",
+    categoria:"ANIMAIS"
+},
+palavra044 = {
+    nome: "PERU",
+    categoria:"ANIMAIS"
+},
+palavra045 = {
+    nome: "ZEBRA",
+    categoria:"ANIMAIS"
+},
+palavra046 = {
+    nome: "DROMEDARIO",
+    categoria:"ANIMAIS"
+},
+palavra047 = {
+    nome: "CALANGO",
+    categoria:"ANIMAIS"
+},
+palavra048 = {
+    nome: "SAGUI",
+    categoria:"ANIMAIS"
+},
+palavra049 = {
+    nome: "LAGARTIXA",
+    categoria:"ANIMAIS"
+},
+palavra050 = {
+    nome: "HIPOPOTAMO",
+    categoria:"ANIMAIS"
+},
+palavra051 = {
+    nome: "A ERA DO GELO",
+    categoria:"TV E CINEMA"
+},
+palavra052 = {
+    nome: "HOMEM ARANHA",
+    categoria:"TV E CINEMA"
+},
+palavra053 = {
+    nome: "CASA MONSTRO",
+    categoria:"TV E CINEMA"
+},
+palavra054 = {
+    nome: "TELA QUENTE",
+    categoria:"TV E CINEMA"
+},
+palavra055 = {
+    nome: "STRANGER THINGS",
+    categoria:"TV E CINEMA"
+},
+palavra056 = {
+    nome: "O REI DO GADO",
+    categoria:"TV E CINEMA"
+},
+palavra057 = {
+    nome: "MULHER MARAVILHA",
+    categoria:"TV E CINEMA"
+},
+palavra058 = {
+    nome: "O INCRIVEL HULK",
+    categoria:"TV E CINEMA"
+},
+palavra059 = {
+    nome: "BOB ESPONJA",
+    categoria:"TV E CINEMA"
+},
+palavra060 = {
+    nome: "PANICO NA TV",
+    categoria:"TV E CINEMA"
+}
+
+];
+
+
+criarPalavraSecreta();
+function criarPalavraSecreta(){
+    const indexPalavra = parseInt(Math.random() * palavras.length)
+    
+    palavraSecretaSorteada = palavras[indexPalavra].nome;
+    palavraSecretaCategoria = palavras[indexPalavra].categoria;
+    console.log(palavraSecretaSorteada)
+    console.log(palavraSecretaCategoria)
+}
+
+
+montarPalavraNaTela()
+function montarPalavraNaTela(){
+  const categoria = document.getElementById("categoria");
+  categoria.innerHTML = palavraSecretaCategoria
+
+  const palavraTela = document.getElementById("palavra-secreta");
+  palavraTela.innerHTML = "";
+
+  for( i=0; i < palavraSecretaSorteada.length; i++){
+    if(listaDinamica[i] == undefined){
+      listaDinamica[i] = "&nbsp;"
+      palavraTela.innerHTML =palavraTela.innerHTML + "<div class='letras'>"+ listaDinamica[i] +"</div>"    }
+      else{
+        palavraTela.innerHTML =palavraTela.innerHTML + "<div class='letras'>"+ listaDinamica[i] +"</div>" 
+      }
+  }
+
+
+}
+
+function verificarLetraEscolhida(letra){
+  document.getElementById("tecla-" + letra).disabled =true
+  if(tentativas > 0)
+  {
+  mudarStyleLetra("tecla-" + letra)
+  comparaListas(letra);
+  montarPalavraNaTela();
+  }
+}
+
+function mudarStyleLetra(tecla){
+  document.getElementById(tecla).style.background = "#C71585";
+  document.getElementById(tecla).style.color = "#ffffff";
+  
+}
+
+function comparaListas(letra){
+  const pos = palavraSecretaSorteada.indexOf(letra)
+  if(pos < 0){
+    tentativas--
+    // apararecer imagens
+    carregaImagemForca();
+    // verificar se ainda tem tentativas // mensagens
+    if (tentativas ==  0){
+
+      abreModal("OPS!", "Não foi dessa vez ... A palavra secreta era <br>" + palavraSecretaSorteada);
+    }
+  }
+  else{
+    for (i = 0; i < palavraSecretaSorteada.length; i++ ){
+       if(palavraSecretaSorteada[i]== letra){
+         listaDinamica[i] =letra;
+       }
+    }
+  }
+let vitoria = true;
+for (i = 0; i  < palavraSecretaSorteada.length; i ++){
+  if(palavraSecretaSorteada[i] != listaDinamica[i]){
+    vitoria = false;
+  }
+} 
+if(vitoria == true){
+  // mensagem na tela 
+  abreModal("PARABÉNS!", "Você venceu");
+  tentativas = 0;
+}
+}
+
+function carregaImagemForca(){
+  switch(tentativas){
+    case 5:
+      document.getElementById("imagem").style.background="url('../img/forca01.png')";
+      break;
+
+      case 4:
+      document.getElementById("imagem").style.background="url('../img/forca02.png')";
+      break;
+      
+      case 3:
+      document.getElementById("imagem").style.background="url(../img/forca03.png)";
+      break;
+
+      case 2:
+      document.getElementById("imagem").style.background="url(../img/forca04.png)";
+      break;
+
+      case 1:
+      document.getElementById("imagem").style.background="url(../img/forca05.png)";
+      break;
+
+      case 0:
+      document.getElementById("imagem").style.background="url(../img/forca06.png)";
+      break;
+
+      default:
+        document.getElementById("imagem").style.background="url(../img/forca.png)";
+      break;
+  }
+};
+
+
+function abreModal( titulo, mensagem){
+  let modalTitulo = document.getElementById("exampleModalLabel");
+  modalTitulo.innerHTML = titulo;
+
+  let modalBody = document.getElementById("modalBody");
+  modalBody.innerHTML = mensagem;
+   $("#myModal").modal({
+     show: true
+   })
+}
+
+
+let bntReiniciar = document.querySelector("#btnReiniciar")
+bntReiniciar.addEventListener("click", function(){
+    location.reload();
+});
+
+let btnJogarNovamente = document.querySelector("#btnJogarNovamente")
+bntReiniciar.addEventListener("click", function(){
+    location.reload();
+});
